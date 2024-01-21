fb950f81a67f454c9ca704c7563cda49e3570e94
gmeyer24 <144490098+gmeyer24@users.noreply.github.com>
2024-01-21 13:09:10 -0600

    cleaned up forms and changed post to show in desc order

--- controllers/page-routes.js
+++ controllers/page-routes.js
@@ -1,5 +1,4 @@
 const router = require("express").Router();
-const { log } = require("console");
 const { Post, Comment, User } = require("../models");
 const withAuth = require("../utils/auth");
 
@@ -7,8 +6,9 @@ const withAuth = require("../utils/auth");
 router.get("/", async (req, res) => {
   try {
     const dbPostData = await Post.findAll({
-      include: [{ model: User, attributes: ['username'] }],
-  });
+      include: [{ model: User, attributes: ["username"] }],
+      order: [["createdAt", "DESC"]],
+    });
 
     const posts = dbPostData.map((post) => post.get({ plain: true }));
     console.log("BLOGS", posts);
@@ -53,12 +53,13 @@ router.get("/post/:id", async (req, res) => {
 });
 
 // Get Dashboard data.. user's blog posts
-router.get("/dashboard", async (req, res) => {
+router.get("/dashboard", withAuth, async (req, res) => {
   try {
     const postData = await Post.findAll({
       where: {
         user_id: req.session.user_id,
       },
+      order: [["createdAt", "DESC"]],
     });
 
     const posts = postData.map((dashboard) => dashboard.get({ plain: true }));
@@ -84,45 +85,22 @@ router.get("/dashboard/edit/:id", async (req, res) => {
         {
           model: User,
           attributes: ["username"],
-        }]
-      });
+        },
+      ],
+    });
 
     if (!dbPostData) {
-      res.status(404).json({ message: 'Post not found' });
+      res.status(404).json({ message: "Post not found" });
       return;
     }
 
     const post = dbPostData.get({ plain: true });
-  res.render("edit-post", {post, loggedIn: req.session.loggedIn})
-} catch (err) {
-  console.log(err);
-  res.status(500).json(err);
-}
+    res.render("edit-post", { post, loggedIn: req.session.loggedIn });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
-
-// // Get form to update post
-// router.get("/dashboard/edit/:id/update", async (req, res) => {
-//   try {
-//     const dbPostData = await Post.findByPk(req.params.id, {
-//       include: [
-//         {
-//           model: User,
-//           attributes: ["username"],
-//         }]
-//       });
-
-//     if (!dbPostData) {
-//       res.status(404).json({ message: 'Post not found' });
-//       return;
-//     }
-
-//     const post = dbPostData.get({ plain: true });
-//   res.render("update-post", {post, loggedIn: req.session.loggedIn})
-// } catch (err) {
-//   console.log(err);
-//   res.status(500).json(err);
-// }
-// });
 
 // Login route
 router.get("/login", (req, res) => {
