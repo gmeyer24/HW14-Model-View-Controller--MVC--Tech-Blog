1841d35c074b8b50c0f511240c2c1c8def576f61
gmeyer24 <144490098+gmeyer24@users.noreply.github.com>
2024-01-22 22:56:47 -0600

    clear seed data since it is not being used.

--- seeds/postData.js
+++ seeds/postData.js
@@ -2,23 +2,7 @@
 
 const { Post } = require('../models');
 
-const postdata = [
-  {
-    title: 'Why MVC is so important',
-    posted_date:"2024-01-16",
-    author: 'Gavin',
-    contents: 'This is the most important.',
-    blog_id: 1,
-  },
-  {
-    title: 'Second Post',
-    posted_date:"2024-01-16",
-    author: 'Gavin',
-    contents: 'Follow this post for tips on coding.',
-    blog_id: 2,
-  },
- 
-];
+const postdata = [];
 
 const seedPosts = () => Post.bulkCreate(postdata);
 
